5268462aa8b4771cafd32ebd80abf4eed0aa45d0
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2019-08-19 10:28:48 +0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/my-personal-web/'
-      : '/'
-  }
+    publicPath: process.env.NETLIFY === 'true' ? '/' :
+        process.env.NODE_ENV === 'production' ? '/my-personal-web/'
+            : '/'
+}
