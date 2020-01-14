430c0ecb167e6718aa01ca6a30714f1315072b54
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2020-01-14 14:12:37 +0700

    Change navbar to buefy navbar

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,39 +1,19 @@
 <template>
-  <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
-    <div class="container">
-      <div class="navbar-brand">
-        <router-link class="navbar-item" to="/">
+  <b-navbar fixed-top type="is-primary">
+      <template slot="brand">
+        <b-navbar-item tag="router-link" to="/">
           <img src="../assets/my-logo.png" />
-        </router-link>
-        <a
-          id="burger"
-          role="button"
-          class="navbar-burger burger"
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbarBasicExample"
-          @click="burgerClick"
-        >
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-      <div id="navbarBasicExample" class="navbar-menu">
-        <div class="navbar-end">
-          <router-link class="navbar-item" to="/">Home</router-link>
-          <router-link class="navbar-item" to="/education">Education</router-link>
-          <router-link class="navbar-item" to="/experience">Experience</router-link>
-          <router-link class="navbar-item" to="/portofolio">Portofolio</router-link>
-          <router-link class="navbar-item" to="/projects">Projects</router-link>
-          <router-link class="navbar-item" to="/about">About</router-link>
-        </div>
-      </div>
-    </div>
-  </nav>
+        </b-navbar-item>
+      </template>
+      <template slot="end">
+          <b-navbar-item tag="router-link" to="/">Home</b-navbar-item>
+          <b-navbar-item tag="router-link" to="/education">Education</b-navbar-item>
+          <b-navbar-item tag="router-link" to="/experience">Experience</b-navbar-item>
+          <b-navbar-item tag="router-link" to="/portofolio">Portofolio</b-navbar-item>
+          <b-navbar-item tag="router-link" to="/projects">Projects</b-navbar-item>
+          <b-navbar-item tag="router-link" to="/about">About</b-navbar-item>
+      </template>
+  </b-navbar>
 </template>
-
-<script lang="ts" scoped src="./MyHeader.ts">
-</script>
 
 
