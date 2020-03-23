743bb2c41776c91cd4109d3b325b6fc8d2b62afa
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2020-03-23 07:52:50 +0700

    Change button to small and remove card

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="content">
-    <h2>Contact</h2>    
+    <h2>Contact</h2>
     <div class="columns is-multiline">
       <div class="column">
         <div class="card">
@@ -14,39 +14,47 @@
             <p>Web Development & Mobile Development</p>
             <h5>Address</h5>
             <b-icon pack="fa" icon="home" size="is-small"></b-icon>
-            <span>Jalan Cipaku Indah VIII, Cidadap, Bandung</span>
+            <span>Bandung</span>
           </div>
         </div>
       </div>
       <div class="column">
-        <div class="card">
-          <div class="card-content">
-            <h3>Media Social</h3>
-            <p class="buttons are-medium">
-              <a href="mailto:bervianto.leo@gmail.com" class="button is-info is-outlined">
-                <span class="icon">
-                  <b-icon pack="fa" icon="envelope"></b-icon>
-                </span>
-                <span>Email</span>
-              </a>
-              <a
-                :href="contact.address"
-                class="button is-info is-outlined"
-                v-bind:key="contact.id"
-                v-for="contact in contacts"
-              >
-                <span class="icon">
-                  <b-icon pack="fab" :icon="contact.icon"></b-icon>
-                </span>
-                <span>{{contact.title}}</span>
-              </a>
-            </p>
-          </div>
+        <div class="content">
+          <h3 class="title">Media Social</h3>
+          <p class="buttons are-small">
+            <a href="mailto:bervianto.leo@gmail.com" class="button is-info is-outlined">
+              <span class="icon">
+                <b-icon pack="fa" icon="envelope"></b-icon>
+              </span>
+              <span>Email</span>
+            </a>
+            <a
+              :href="contact.address"
+              class="button is-info is-outlined"
+              v-bind:key="contact.id"
+              v-for="contact in contacts"
+            >
+              <span class="icon">
+                <b-icon pack="fab" :icon="contact.icon"></b-icon>
+              </span>
+              <span>{{contact.title}}</span>
+            </a>
+          </p>
         </div>
       </div>
       <div class="column">
         <h3>Certification</h3>
-        <div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="61a336e9-92a7-4c8c-aeee-a5c07bf3fae5" data-share-badge-host="https://www.youracclaim.com"></div><script type="application/javascript" defer src="//cdn.youracclaim.com/assets/utilities/embed.js"></script>
+        <div
+          data-iframe-width="150"
+          data-iframe-height="270"
+          data-share-badge-id="61a336e9-92a7-4c8c-aeee-a5c07bf3fae5"
+          data-share-badge-host="https://www.youracclaim.com"
+        ></div>
+        <script
+          type="application/javascript"
+          defer
+          src="//cdn.youracclaim.com/assets/utilities/embed.js"
+        ></script>
       </div>
     </div>
   </div>
