c0bf862a5d5ee82c2aeae20f41ba132ca82e5789
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2020-09-25 00:40:43 +0700

    Update card for project

--- src/components/Github.vue
+++ src/components/Github.vue
@@ -4,19 +4,31 @@
       <b-icon pack="fas" icon="sync-alt" size="is-large" custom-class="fa-spin"></b-icon>
     </b-loading>
     <div v-for="data in myData" v-bind:key="data.id" class="column is-half">
-      <div class="card">
+      <b-collapse class="card" animation="slide" aria-id="contentIdForA11y3">
+        <div
+          slot="trigger"
+          slot-scope="props"
+          class="card-header"
+          role="button"
+          aria-controls="contentIdForA11y3"
+        >
+          <div class="card-header-title">
+            <p class="title is-4">
+              <a v-bind:href="data.svn_url">
+                {{data.name}} |
+                <b-icon icon="star" type="is-warning"></b-icon>
+                : {{data.stargazers_count}}
+              </a>
+            </p>
+          </div>
+          <a class="card-header-icon">
+            <b-icon :icon="props.open ? 'angle-down' : 'angle-up'"></b-icon>
+          </a>
+        </div>
         <div class="card-content">
-          <p class="title is-4">
-            <a v-bind:href="data.svn_url">{{data.name}}</a>
-          </p>
-          <p class="subtitle is-6">
-            Stars
-            <b-icon icon="star" type="is-warning"></b-icon>
-            : {{data.stargazers_count}}
-          </p>
           <div class="content">{{data.description}}</div>
         </div>
-      </div>
+      </b-collapse>
     </div>
   </div>
 </template>
