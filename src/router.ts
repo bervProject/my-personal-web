57fe105fed12426847115accc76a34d9d54179fd
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2020-12-29 09:11:02 +0700

    Split home route

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
 
 Vue.use(Router);
 
@@ -10,7 +9,8 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home,
+      component: () =>
+        import(/* webpackChunkName: "home" */ './views/Home.vue'),
     },
     {
       path: '/education',
