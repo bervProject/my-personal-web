5ab5500e8e8f773391985c2db1063cd40e563a65
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2020-12-30 12:06:57 +0700

    Update footer

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -1,24 +1,50 @@
 <template>
   <footer class="footer">
-    <div class="content has-text-black has-text-centered">
-      <p>
-        ©
-        <b>My Personal Web</b> made with
-        <b-icon icon="heart" type="is-red"></b-icon>by
-        <a href="https://berviantoleo.github.io/">Bervianto Leo Pratama</a>.
-      </p>
-      <p>
-        The source code is licensed
-        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The
-        website content is licensed
-        <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/"
-          >CC BY NC SA 4.0</a
-        >.
-      </p>
-      <p>
-        Version
-        <b>{{ version }}</b>
-      </p>
+    <div class="container">
+      <div class="columns">
+        <div class="column">
+          <div class="content has-text-black has-text-centered">
+            <p>
+              ©
+              <b>My Personal Web</b> made with
+              <b-icon icon="heart" type="is-red"></b-icon>by
+              <a href="https://berviantoleo.github.io/">Bervianto Leo Pratama</a
+              >.
+            </p>
+            <p>
+              The source code is licensed
+              <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
+              The website content is licensed
+              <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/"
+                >CC BY NC SA 4.0</a
+              >.
+            </p>
+            <p>
+              Version
+              <b>{{ version }}</b>
+            </p>
+          </div>
+        </div>
+        <div class="column">
+          <div class="content has-text-black has-text-centered">
+            <p>
+              <a here="https://buefy.org/">
+                #MadeWithBuefy
+              </a>
+            </p>
+            <p>
+              <a href="https://bulma.io">
+                <img
+                  src="https://bulma.io/images/made-with-bulma--black.png"
+                  alt="Made with Bulma"
+                  width="128"
+                  height="24"
+                />
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   </footer>
 </template>
