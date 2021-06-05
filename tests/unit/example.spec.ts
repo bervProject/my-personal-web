b3117faf8ca08cd505ee77ddcb9de13b5ae4224b
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-06-06 00:52:33 +0700

    Temporary changes to jest

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,13 +1,10 @@
-import { expect } from 'chai';
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import MyHeader from '../../src/components/MyHeader.vue';
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).to.include(msg);
+describe('MyHeader.vue', () => {
+  it('Render correctly', () => {
+    const msg = 'Experience';
+    const wrapper = shallowMount(MyHeader);
+    expect(wrapper.text()).toContain(msg);
   });
 });
