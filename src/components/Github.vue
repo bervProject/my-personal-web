bd9ebfd93d429c4b8f060852cf65a0fddcd811fb
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-10-10 12:30:57 +0700

    Add topics & archived

--- src/components/Github.vue
+++ src/components/Github.vue
@@ -13,6 +13,13 @@
       <b-table-column field="name" label="Repo" v-slot="props">
         {{ props.row.name }}
       </b-table-column>
+      <b-table-column field="topics" label="Topics" v-slot="props">
+        <b-taglist>
+          <b-tag v-for="topic in props.row.topics" :key="topic">
+            {{ topic }}
+          </b-tag>
+        </b-taglist>
+      </b-table-column>
       <b-table-column field="stars" label="Stars" v-slot="props">
         {{ props.row.stargazers_count }}
       </b-table-column>
@@ -22,6 +29,15 @@
           icon="check"
           type="is-success"
           v-if="props.row.fork"
+        />
+        <b-icon v-else pack="fas" icon="times" type="is-danger" />
+      </b-table-column>
+      <b-table-column field="archived" label="Archived" v-slot="props">
+        <b-icon
+          pack="fas"
+          icon="check"
+          type="is-success"
+          v-if="props.row.archived"
         />
         <b-icon v-else pack="fas" icon="times" type="is-danger" />
       </b-table-column>
