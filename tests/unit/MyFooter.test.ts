a28a0dd3b9e2dbba08ff92c9db6c7867d9a3278d
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-10-13 00:41:26 +0700

    Update test

--- tests/unit/MyFooter.test.ts
+++ tests/unit/MyFooter.test.ts
@@ -1,10 +1,17 @@
+import messages from '../../src/messages';
 import { shallowMount } from '@vue/test-utils';
 import { createLocalVue } from '@vue/test-utils';
 import Buefy from 'buefy';
+import VueI18n from 'vue-i18n';
 // create an extended `Vue` constructor
 const localVue = createLocalVue()
 // install plugins as normal
 localVue.use(Buefy);
+localVue.use(VueI18n);
+const i18n = new VueI18n({
+  locale: 'en', // set locale
+  messages, // set locale messages
+});
 
 import MyFooter from '../../src/components/MyFooter.vue';
 
@@ -14,6 +21,7 @@ describe('MyFooter.vue', () => {
     const msg = currentVersion;
     const wrapper = shallowMount(MyFooter, {
       localVue,
+      i18n,
       stubs: ['router-link', 'router-view']
     });
     expect(wrapper.text()).toContain(msg);
@@ -24,6 +32,7 @@ describe('MyFooter.vue', () => {
     const msg = '0';
     const wrapper = shallowMount(MyFooter, {
       localVue,
+      i18n,
       stubs: ['router-link', 'router-view']
     });
     expect(wrapper.text()).toContain(msg);
--- tests/unit/MyHeader.test.ts
+++ tests/unit/MyHeader.test.ts
@@ -1,10 +1,19 @@
 import { shallowMount } from '@vue/test-utils';
 import { createLocalVue } from '@vue/test-utils';
 import Buefy from 'buefy';
+import VueI18n from 'vue-i18n';
+import messages from '../../src/messages';
+
 // create an extended `Vue` constructor
 const localVue = createLocalVue()
 // install plugins as normal
 localVue.use(Buefy);
+localVue.use(Buefy);
+localVue.use(VueI18n);
+const i18n = new VueI18n({
+  locale: 'en', // set locale
+  messages, // set locale messages
+});
 
 import MyHeader from '../../src/components/MyHeader.vue';
 
@@ -13,6 +22,7 @@ describe('MyHeader.vue', () => {
     const msg = 'Experience';
     const wrapper = shallowMount(MyHeader, {
       localVue,
+      i18n,
       stubs: ['router-link', 'router-view']
     });
     expect(wrapper.text()).toContain(msg);
