fd605c0441dd26faa95a1134941b752cf616b8ab
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-12-30 14:40:06 +0700

    move contacts to json data

--- src/views/About.ts
+++ src/views/About.ts
@@ -1,90 +1,27 @@
 import { Component, Vue } from 'vue-property-decorator';
+import { get } from '@/services';
 
 @Component({
   name: 'About',
   metaInfo: {
-    title: 'About Me'
+    title: 'About Me',
   },
 })
 export default class About extends Vue {
-  public contacts: object[] = [
-    {
-      id: 1,
-      title: 'Github',
-      icon: 'github',
-      address: 'https://github.com/berviantoleo',
-    },
-    {
-      id: 2,
-      title: 'Gitlab',
-      icon: 'gitlab',
-      address: 'https://gitlab.com/bervianto.leo',
-    },
-    {
-      id: 3,
-      title: 'Linkedin',
-      icon: 'linkedin',
-      address: 'https://www.linkedin.com/in/bervianto-leo-pratama',
-    },
-    {
-      id: 4,
-      title: 'Medium',
-      icon: 'medium',
-      address: 'https://berviantoleo.medium.com/',
-    },
-    {
-      id: 5,
-      title: 'Dev',
-      icon: 'dev',
-      address: 'https://dev.to/berviantoleo',
-    },
-    {
-      id: 6,
-      title: 'Telegram',
-      icon: 'telegram',
-      address: 'https://t.me/berviantoleo',
-    },
-    {
-      id: 7,
-      title: 'Stack Overflow',
-      icon: 'stack-overflow',
-      address: 'https://stackoverflow.com/users/6948591/bervianto-leo-pratama',
-    },
-    {
-      id: 8,
-      title: 'Youtube',
-      icon: 'youtube',
-      address: 'https://youtube.com/c/BerviantoLeoPratama',
-    },
-    {
-      id: 9,
-      title: 'Twitter',
-      icon: 'twitter',
-      address: 'https://twitter.com/berviantoleo',
-    },
-    {
-      id: 10,
-      title: 'Facebook',
-      icon: 'facebook',
-      address: 'https://www.facebook.com/bervianto.leo',
-    },
-    {
-      id: 11,
-      title: 'Instagram',
-      icon: 'instagram',
-      address: 'https://www.instagram.com/bervianto.leo',
-    },
-    {
-      id: 12,
-      title: 'Steam',
-      icon: 'steam',
-      address: 'https://steamcommunity.com/id/cyber-name',
-    },
-    {
-      id: 13,
-      title: 'Soundcloud',
-      icon: 'soundcloud',
-      address: 'https://soundcloud.com/bervianto-leo-pratama',
-    },
-  ];
+  public contacts: object[] = [];
+  public isLoading: boolean = false;
+
+  mounted(): void {
+    this.isLoading = true;
+    get('berviantoleo/bervdata/contacts')
+      .then(result => {
+        this.contacts = result.data;
+      })
+      .catch(err => {
+        console.error(err);
+      })
+      .finally(() => {
+        this.isLoading = false;
+      });
+  }
 }
