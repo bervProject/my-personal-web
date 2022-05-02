95135a03fea75756e69d7b05c5aea3bd933a655b
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2022-05-02 22:19:13 +0700

    Add new songs

--- src/views/FavoriteAudio.ts
+++ src/views/FavoriteAudio.ts
@@ -75,6 +75,10 @@ export default class FavoriteAudio extends Vue {
       srcAudio:
         'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/172156106&color=%23cccfd3&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
     },
+    {
+      id: 14,
+      srcAudio: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1066587745&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'
+    }
   ];
   public currentPage: number = 1;
   public perPage: number = 6;
--- src/views/SongsPage.ts
+++ src/views/SongsPage.ts
@@ -8,12 +8,15 @@ import { Component, Vue } from 'vue-property-decorator';
 })
 export default class SongsPage extends Vue {
   public trackData : string[] = [
+    'https://open.spotify.com/embed/track/0ZUoF50WvrxucLmFKxuxv6',
+    'https://open.spotify.com/embed/track/4lu7xSX0x9gRmG1QAApWQN',
     'https://open.spotify.com/embed/track/3NyxWuYU0s2GA6oZgDRQep',
     'https://open.spotify.com/embed/track/4eO2k6hVljgRNs61OcRV7g',
-    'https://open.spotify.com/embed/track/7u83tMqc2n4L1FuShWTqGH',
-    'https://open.spotify.com/embed/track/0ZUoF50WvrxucLmFKxuxv6'
+    'https://open.spotify.com/embed/track/7u83tMqc2n4L1FuShWTqGH'
+
   ];
   public youtubeData: string[] = [
-    'https://www.youtube.com/embed/CkQ8zSDlJmk'
+    'https://www.youtube.com/embed/CkQ8zSDlJmk',
+    'https://www.youtube.com/embed/2fissTmiQOU'
   ]
 }
