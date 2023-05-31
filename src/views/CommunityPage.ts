6b2638c353ca108aea0ec8287c1c040f6f03173d
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2023-05-31 08:04:41 +0700

    fix: add community

--- src/views/CommunityPage.ts
+++ src/views/CommunityPage.ts
@@ -11,6 +11,10 @@ export default {
                     title: "External Code Reviewer - Dicoding",
                     link: "https://www.dicoding.com/users/berviantoleo",
                 }
+            ],
+            communityList: [
+                "4c1544dc-271b-404e-974a-f991320ab9d8",
+                "8cad11b0-12d7-4193-b51a-11a0c75de467"
             ]
         }
     }
