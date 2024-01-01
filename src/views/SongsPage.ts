4539a1c0e806dbd2feb7b3d94fafb07897b7bf51
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2024-01-01 22:23:05 +0700

    Update SongsPage.ts

--- src/views/SongsPage.ts
+++ src/views/SongsPage.ts
@@ -10,8 +10,11 @@ export default defineComponent({
         'https://open.spotify.com/embed/track/0ZUoF50WvrxucLmFKxuxv6',
         'https://open.spotify.com/embed/track/4lu7xSX0x9gRmG1QAApWQN',
         'https://open.spotify.com/embed/track/3NyxWuYU0s2GA6oZgDRQep',
-        'https://open.spotify.com/embed/track/4eO2k6hVljgRNs61OcRV7g',
-        'https://open.spotify.com/embed/track/7u83tMqc2n4L1FuShWTqGH'
+        'https://open.spotify.com/embed/track/3FYIfyziD8T4CvxnpLePb4',
+        'https://open.spotify.com/embed/track/7jLNptdHMIjgnd46Up3sx4',
+        'https://open.spotify.com/embed/track/7ePyrOcnlE6KPWgsy2ouQB',
+        'https://open.spotify.com/embed/track/0zQE5JOKWYFg2hQWWhuChj',
+        'https://open.spotify.com/embed/track/4q4Uzzu5VOOUFLjDDLsruS'
       ],
       youtubeData: [
         'https://www.youtube.com/embed/CkQ8zSDlJmk',
