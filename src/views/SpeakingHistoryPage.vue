dc914642b51ce9024ffa46704566d7b1f2939bde
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2024-02-08 14:15:29 +0700

    fix: add seasonize

--- src/views/SpeakingHistoryPage.vue
+++ src/views/SpeakingHistoryPage.vue
@@ -4,22 +4,31 @@
             Public Speaking Histories
         </h2>
         <div class="columns">
-            <o-table :data="speakingData" >
-                <o-table-column field="event" label="Event" v-slot="props">
-                    {{ props.row.event }}
-                </o-table-column>
-                <o-table-column field="topics" label="Topics" v-slot="props">
-                    {{ props.row.topics }}
-                </o-table-column>
-                <o-table-column field="date" label="Date" v-slot="props">
-                    {{ showDate(props.row.date) }}
-                </o-table-column>
-                <o-table-column field="link" label="Link" v-slot="props">
-                    <a class="button is-primary" :href="props.row.link">Visit</a>
-                </o-table-column>
-            </o-table>
+            <div class="column">
+                <h3 class="subtitle">Details</h3>
+                <o-table :data="speakingData" >
+                    <o-table-column field="event" label="Event" v-slot="props">
+                        {{ props.row.event }}
+                    </o-table-column>
+                    <o-table-column field="topics" label="Topics" v-slot="props">
+                        {{ props.row.topics }}
+                    </o-table-column>
+                    <o-table-column field="date" label="Date" v-slot="props">
+                        {{ showDate(props.row.date) }}
+                    </o-table-column>
+                    <o-table-column field="link" label="Link" v-slot="props">
+                        <a class="button is-primary" :href="props.row.link">Visit</a>
+                    </o-table-column>
+                </o-table>
+                <o-button tag="a" variant="primary" href="https://berviantoleo.notion.site/Speaker-Experiences-e2a28dba41c94e158d5dc3d901d8bb17">More Details</o-button>
+            </div>
         </div>
-        <o-button tag="a" variant="primary" href="https://berviantoleo.notion.site/Speaker-Experiences-e2a28dba41c94e158d5dc3d901d8bb17">More Details</o-button>
+        <div class="columns">
+            <div class="column">
+                <h3 class="subtitle">Seasonize</h3>
+                <iframe src="./seasonize.html" width="100%" height="300" style="border:none;"></iframe>
+            </div>
+        </div>
     </div>
 </template>
 <script lang="ts">
