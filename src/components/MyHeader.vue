c07d8e510a4036d674522784a51e7f827634bcf9
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2024-07-23 19:41:58 +0700

    Update MyHeader.vue

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="container">
-    <router-link to="/" class="title m-2">Home</router-link>
-    <router-link to="/songs" class="title m-2">Favourite Songs</router-link>
+  <div class="container mt-4">
+    <router-link to="/" class="title m-3">| Home |</router-link>
+    <router-link to="/songs" class="title m-3">| Favourite Songs |</router-link>
   </div>
 </template>
 
