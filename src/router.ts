76cc6feb739c08010c07fdcf4c0bc4fdd283a960
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2025-06-17 08:56:27 +0000

    fix: fix build

--- src/router.ts
+++ src/router.ts
@@ -1,66 +1,65 @@
-import { RouteRecordRaw, createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
-// eslint-disable-next-line
-const anotherRoutes: RouteRecordRaw[] = [
-  {
-    path: '/community/blog',
-    name: 'blog',
-    component: () =>
-      import(/* webpackChunkName: "blog" */ './views/BlogPage.vue'),
-  },
-  {
-    path: '/donate',
-    name: 'donate',
-    component: () =>
-      import(/* webpackChunkName: "donate" */ './views/DonatePage.vue'),
-  },
-  {
-    path: '/experience',
-    name: 'experience',
-    component: () =>
-      import(/* webpackChunkName: "experience" */ './views/ExperiencePage.vue'),
-  },
-  {
-    path: '/projects',
-    name: 'projects',
-    component: () =>
-      import(/* webpackChunkName: "projects" */ './views/ProjectsPage.vue'),
-  },
-  {
-    path: '/portofolio',
-    name: 'portofolio',
-    component: () =>
-      import(/* webpackChunkName: "portofolio" */ './views/PortofolioPage.vue'),
-  },
-  {
-    path: '/favorite-audio',
-    name: 'favorite-audio',
-    component: () =>
-      import(
-          /* webpackChunkName: "favorite-audio" */ './views/FavoriteAudio.vue'
-      ),
-  },
-  {
-    path: '/about',
-    name: 'about',
-    component: () =>
-      import(/* webpackChunkName: "about" */ './views/AboutPage.vue'),
-  },
-  {
-    path: '/community/speaking',
-    name: 'speaking',
-    component: () =>
-      import(/* webpackChunkName: "speakers" */ './views/SpeakingHistoryPage.vue'),
-  },
-  {
-    path: '/community',
-    name: 'community',
-    component: () =>
-      import(/* webpackChunkName: "community" */ './views/CommunityPage.vue'),
-  },
-];
+// const anotherRoutes: RouteRecordRaw[] = [
+//   {
+//     path: '/community/blog',
+//     name: 'blog',
+//     component: () =>
+//       import(/* webpackChunkName: "blog" */ './views/BlogPage.vue'),
+//   },
+//   {
+//     path: '/donate',
+//     name: 'donate',
+//     component: () =>
+//       import(/* webpackChunkName: "donate" */ './views/DonatePage.vue'),
+//   },
+//   {
+//     path: '/experience',
+//     name: 'experience',
+//     component: () =>
+//       import(/* webpackChunkName: "experience" */ './views/ExperiencePage.vue'),
+//   },
+//   {
+//     path: '/projects',
+//     name: 'projects',
+//     component: () =>
+//       import(/* webpackChunkName: "projects" */ './views/ProjectsPage.vue'),
+//   },
+//   {
+//     path: '/portofolio',
+//     name: 'portofolio',
+//     component: () =>
+//       import(/* webpackChunkName: "portofolio" */ './views/PortofolioPage.vue'),
+//   },
+//   {
+//     path: '/favorite-audio',
+//     name: 'favorite-audio',
+//     component: () =>
+//       import(
+//           /* webpackChunkName: "favorite-audio" */ './views/FavoriteAudio.vue'
+//       ),
+//   },
+//   {
+//     path: '/about',
+//     name: 'about',
+//     component: () =>
+//       import(/* webpackChunkName: "about" */ './views/AboutPage.vue'),
+//   },
+//   {
+//     path: '/community/speaking',
+//     name: 'speaking',
+//     component: () =>
+//       import(/* webpackChunkName: "speakers" */ './views/SpeakingHistoryPage.vue'),
+//   },
+//   {
+//     path: '/community',
+//     name: 'community',
+//     component: () =>
+//       import(/* webpackChunkName: "community" */ './views/CommunityPage.vue'),
+//   },
+// ];
 
-const homeRoute: RouteRecordRaw[] = [
+const homeRoute = [
   {
     path: '/',
     name: 'home',
