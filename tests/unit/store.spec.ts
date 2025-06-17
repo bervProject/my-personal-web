ba4ac41e4e435b5f2b16fca2755899e26eecd06f
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2025-06-17 09:06:42 +0000

    fix: fix coverage

--- tests/unit/store.spec.ts
+++
@@ -1,9 +0,0 @@
-import { expect, describe, it } from 'vitest';
-
-import store from '@/store';
-
-describe('store.ts', () => {
-  it('Check attributes', () => {
-    expect(store.state).toEqual({});
-  });
-});
