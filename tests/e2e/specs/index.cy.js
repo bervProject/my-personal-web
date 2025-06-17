ead535d78c7adde217dd268ce2c9f3cdf5be4362
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2025-06-17 09:56:09 +0000

    fix: integration test

--- tests/e2e/specs/index.cy.js
+++ tests/e2e/specs/index.cy.js
@@ -3,6 +3,6 @@
 describe('My First Test', () => {
   it('Visits the app root url', () => {
     cy.visit('/')
-    cy.contains('p', 'Bervianto Leo Pratama')
+    cy.contains('strong', 'Bervianto Leo Pratama')
   })
 })
