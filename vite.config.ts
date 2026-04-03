55e45b1a6053f48aa90a223c3176c8539e37fba4
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2026-04-03 16:47:46 +0700

    fix vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { browserslistToTargets } from 'lightningcss'
+import browserslist from 'browserslist'
 import {resolve} from 'node:path'
 
 // https://vite.dev/config/
@@ -7,5 +9,15 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: [{ find: "@", replacement: resolve(__dirname, "./src") }]
-  }
+  },
+  css: {
+    transformer: 'lightningcss',
+    lightningcss: {
+      errorRecovery: true,
+      targets: browserslistToTargets(browserslist()),
+    },
+  },
+  build: {
+    cssMinify: 'lightningcss',
+  },
 })
