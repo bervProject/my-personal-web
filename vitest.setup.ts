893a4a72292041e3e52a62d0f2b361149c1b54f4
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2026-05-18 06:09:07 +0700

    Update vitest.setup.ts

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,4 +1,17 @@
 import { config } from '@vue/test-utils'
 import { createI18n } from 'vue-i18n'
+import { beforeEach, vi } from 'vitest';
+
 const i18n = createI18n()
 config.global.plugins = [i18n]
+
+beforeEach(() => {
+  const storage = {};
+  vi.stubGlobal('localStorage', {
+    getItem: vi.fn((key) => storage[key] || null),
+    setItem: vi.fn((key, value) => { storage[key] = value.toString(); }),
+    removeItem: vi.fn((key) => { delete storage[key]; }),
+    clear: vi.fn(() => { Object.keys(storage).forEach(key => delete storage[key]); }),
+  });
+});
+
